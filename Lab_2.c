e6c0dcaf9f6bf3e782d1b16ccec8bced7a132cc0
Ilya <rudlewski@gmail.com>
2018-03-23 23:30:58 +0300

    added the second task

---
+++ Lab_2.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+// structure for files
+typedef struct {
+    char way[PATH_MAX];
+    char name[FILENAME_MAX];
+    int  size;
+} st_file;
+
+char util_name[FILENAME_MAX];
+st_file *records;
+int records_len = 0;
+
+int (*cmpfunc)(st_file file1, st_file file2);
+
+int cmpname(st_file file1, st_file file2) {
+    return (strcmp(file1.name, file2.name) > 0);
+}
+
+int cmpsize(st_file file1, st_file file2) {
+    return ((file1.size - file2.size) < 0);
+}
+
+// function for adding '/'
+char* sls_add(char* way) {
+    if (way[strlen(way) - 1] != '/')
+        strcat(way, "/");
+    return way;
+}
+
+// open directories 
+int dirpass(const char *dir_name) {
+    DIR *directory;
+
+    if (!(directory = opendir(dir_name))) {
+        fprintf(stderr, "%s: %s: %s\n", util_name, dir_name, strerror(errno));
+        return -1;
+    }
+
+    struct dirent *dir_item;
+    while((dir_item = readdir(directory)) != NULL) {
+        char next_item[PATH_MAX];
+        strcpy(next_item, dir_name);
+        strcat(next_item, dir_item->d_name);
+        if(strcmp(".", dir_item->d_name) == 0 ||
+            strcmp("..", dir_item->d_name) == 0)
+            continue;
+
+        struct stat statbuf;
+        lstat(next_item, &statbuf);
+        if(S_ISDIR(statbuf.st_mode)) {
+            dirpass(sls_add(next_item));
+        } else if(S_ISREG(statbuf.st_mode)) {
+            st_file file_tmp;
+            strcpy(file_tmp.name, dir_item->d_name);
+            strcpy(file_tmp.way, next_item);
+            file_tmp.size = (int) statbuf.st_size;
+            records = realloc(records, (++records_len)*sizeof(st_file));
+            records[records_len - 1] = file_tmp;
+        }
+    }
+    closedir(directory);
+}
+
+void flistsort(int sort_opt){
+    if (sort_opt == 1)
+        cmpfunc = cmpname;
+    else
+        cmpfunc = cmpsize;
+
+    int i, j;
+    for (i = 0; i < records_len; i++) {
+        for (j = 0; j < records_len - i - 1; j++) {
+            if (cmpfunc(records[j], records[j+1])) {
+                st_file file_tmp = records[j];
+                records[j] = records[j+1];
+                records[j+1] = file_tmp;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+    if (argc != 4) {
+        fprintf(stderr, "%s\n","Wrong number of arguments");
+        return -1;
+    }
+
+    char dist_path[PATH_MAX];
+    realpath(argv[3], dist_path);
+    sls_add(dist_path);
+
+    char sort_opt = atoi(argv[2]);
+    if (sort_opt != 1 && sort_opt != 2) {
+        fprintf(stderr,"%s\n","Wrong sort option (Must be: 1 or 2)");
+        return -1;
+    }
+
+    char initdir[PATH_MAX];
+    strcpy(initdir, argv[1]);
+    dirpass(sls_add(initdir));
+
+    flistsort(sort_opt);
+
+    if (mkdir(dist_path, 0777) == -1 && errno != EEXIST) {
+        fprintf(stderr, "%s: %s\n", dist_path, strerror(errno));
+        return -1;
+    }
+
+    // add numbers
+    int i, j;
+    for(i = 0; i < records_len; i++) {
+       
+        char add_index[5];
+        sprintf(add_index,"%d",i);
+        strcat(records[i].name,add_index);
+       
+        char dist_file[PATH_MAX + 1];
+        strcpy(dist_file, dist_path);
+        strcat(dist_file, records[i].name);
+
+        if (symlink(records[i].way, dist_file) == -1) 
+           fprintf(stderr, "%s: %s\n",dist_file, strerror(errno));
+        
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -4,6 +4,26 @@ Laboratory task #1.
 
 Write a script that finds all the files of the specified extension in the specified directory and all its subdirectories and creates a hard link for each file found in the specified directory. The file extension and directory for hard links are specified as the first and second command-line arguments.
 
+Laboratory task #2.
+
+Sort in the specified directory (argument 1 of the command line) and in all its subdirectories files by the following criteria (argument 2 of the command line, given as an integer): 1 - by file size, 2 - by file name. To save without saving the directory structure, the sorted files are shared
+list, into the new directory (argument 3 of the command line).
+
+In connection with the indexing of files in directories for file systems ext 2,3,4, before starting the program, it is necessary to temporarily disable the indexing option of the file system as follows:
+sudo tune2fs -O ^ dir_index /dev /sdaXY
+
+Check the result using, ls -l -f.
+
 Лабораторное задание #1.
 
 Написать скрипт, находящий в заданном каталоге и всех его подкаталогах все файлы заданного расширения и создающий для каждого найденного файла жесткую ссылку в заданном каталоге. Расширение файла и каталог для жестких ссылок задаются в качестве первого и второго аргумента командной строки.
+
+Лабораторное задание #2.
+
+Отсортировать в заданном каталоге (аргумент 1 командной строки) и во всех его подкаталогах файлы по следующим критериям (аргумент 2 командной строки, задаётся в виде целого числа): 1 – по размеру файла, 2 – по имени файла. Записать без сохранения структуры каталогов отсортированные файлы общим
+списком, в новый каталог (аргумент 3 командной строки). 
+
+В связи с индексированием файлов в каталогах для файловых систем ext 2,3,4 перед запуском программы необходимо временно отключить опцию индексирования файловой системы следующим образом:
+sudo tune2fs –O ^dir_index /dev/sdaXY
+
+Проверить результат, используя, ls -l –f.
